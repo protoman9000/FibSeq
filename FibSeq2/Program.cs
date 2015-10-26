31af0360d14e99dafa7951b809f0c2e1c6180188
protoman9000 <oyekan900@yahoo.com>
2015-10-25 20:06:34 -0700

    adding in some comments

--- FibSeq2/Program.cs
+++ FibSeq2/Program.cs
@@ -10,20 +10,25 @@ namespace FibSeq2
     {
         static void Main(string[] args)
         {
+            //Constructor
             int tmp = 0;
             int tmp2 = 0;
             int seqNumber = 0;
             int startNumber = 0;
 
+            //Number input
             Console.WriteLine("Enter in the desired integer:");
             string insertNumber = Console.ReadLine();
             int DInterger = Convert.ToInt16(insertNumber);
 
+            //Creating a list to store the number sequence
             List<int> fibList = new List<int>();
             fibList[seqNumber] = tmp2;
             seqNumber++;
             int startNumber2 = tmp + 1;
 
+            //The this is set up is by making startNumber as one, if it pass
+            //the disired interger, we increase the startNumber. 
             for (int x = 0; x < DInterger; x++)
             {
                 int insert = startNumber + startNumber2;
@@ -39,6 +44,8 @@ namespace FibSeq2
                     seqNumber = 1;
                 }
             }
+
+            //printing out the list.
             foreach (int num in fibList)
             {
                 string tmpNum = Convert.ToString(num);
