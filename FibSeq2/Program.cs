3b76194b5040fea79d7cdd68e98b9a1aedf8426c
protoman9000 <oyekan900@yahoo.com>
2015-10-25 20:03:04 -0700

    working on printing out the list

--- FibSeq2/Program.cs
+++ FibSeq2/Program.cs
@@ -10,9 +10,40 @@ namespace FibSeq2
     {
         static void Main(string[] args)
         {
+            int tmp = 0;
+            int tmp2 = 0;
+            int seqNumber = 0;
+            int startNumber = 0;
+
             Console.WriteLine("Enter in the desired integer:");
             string insertNumber = Console.ReadLine();
             int DInterger = Convert.ToInt16(insertNumber);
+
+            List<int> fibList = new List<int>();
+            fibList[seqNumber] = tmp2;
+            seqNumber++;
+            int startNumber2 = tmp + 1;
+
+            for (int x = 0; x < DInterger; x++)
+            {
+                int insert = startNumber + startNumber2;
+                if (insert <= DInterger)
+                {
+                    fibList[seqNumber] = insert;
+                    seqNumber++;
+                }
+                else
+                {
+                    tmp++;
+                    x = 0;
+                    seqNumber = 1;
+                }
+            }
+            foreach (int num in fibList)
+            {
+                string tmpNum = Convert.ToString(num);
+
+            }
         
         }
     }
